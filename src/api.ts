c28f0f221ce8e34cf8bcb990c6d5841cdbdfffae
stuart-p <stuart_palmer85@hotmail.com>
2022-02-13 17:42:41 +0000

    updated slack manifest file. added catch for when no pubs yet exist in list

--- src/api.ts
+++ src/api.ts
@@ -3,17 +3,41 @@ import { IpubData, IpubList } from "./interfaces";
 
 const s3 = new AWS.S3();
 
-export const fetchPubList = async () => {
-  const { Body } = await s3
-    .getObject({
+export const createInitialList = async () => {
+  const blankList = {
+    pubList: [],
+  };
+
+  await s3
+    .putObject({
       Bucket: process.env.BUCKET_NAME ?? "",
       Key: "publist.json",
+      ContentType: "applicaiton/json",
+      Body: JSON.stringify(blankList),
     })
     .promise();
-  if (Body) {
-    return JSON.parse(Body.toString()) as IpubList;
+
+  return blankList;
+};
+
+export const fetchPubList = async () => {
+  try {
+    const { Body } = await s3
+      .getObject({
+        Bucket: process.env.BUCKET_NAME ?? "",
+        Key: "publist.json",
+      })
+      .promise();
+    if (Body) {
+      return JSON.parse(Body.toString()) as IpubList;
+    }
+    throw new Error("returned message was missing data");
+  } catch (err) {
+    if (err.code === "NoSuchKey") {
+      return await createInitialList();
+    }
+    throw new Error("error on fetch");
   }
-  throw new Error("Data not found in Bucket");
 };
 
 export const updatePubList = async (pubEntry: IpubData) => {
